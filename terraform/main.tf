29ee64e912fc5f5557a394af8a472580ee0ec830
Arosha Sumanaweera <s.arosha@directfn.com>
2025-09-22 00:05:09 +1000

    chore: successful appsync test

--- terraform/main.tf
+++ terraform/main.tf
@@ -371,3 +371,11 @@ EOF
 $util.toJson($context.result)
 EOF
 }
+
+output "appsync_api_url" {
+  value = aws_appsync_graphql_api.api.uris["GRAPHQL"]
+}
+
+output "appsync_api_key" {
+  value = aws_appsync_api_key.key.id
+}
